20458503e206c35f58d82c808df0a6e971cef939
unknown <manuel.presentaciongr@gmail.com>
2025-01-31 10:24:03 +0100

    cambios

--- Vista/juegos.php
+++ Vista/juegos.php
@@ -3,7 +3,19 @@ require_once("../Vista/cabecera.html");
 ?>
 
 
-
+<header id="juegos">
+        <h1>AGENDA PERSONAL</h1>
+        <nav>
+            <a href="../Vista/iniAmig.php">Amigos</a>
+            <a href="../Vista/juegos.php">Juegos</a>
+            <a href="../Vista/prestamos.php">Prestamos</a>
+            <a href="../Vista/login.php">Salir</a>
+        </nav>
+        <div>
+            <a href="../Vista/nuevoJuego.php">Insertar Juegos</a>
+            <a href="../Vista/buscJuego.php">Buscar Juegos</a>
+        </div>
+    </header>
 
 
 
--- Vista/prestamos.php
+++ Vista/prestamos.php
@@ -3,7 +3,19 @@ require_once("../Vista/cabecera.html");
 ?>
 
 
-
+<header id="presta">
+        <h1>AGENDA PERSONAL</h1>
+        <nav>
+            <a href="../Vista/iniAmig.php">Amigos</a>
+            <a href="../Vista/juegos.php">Juegos</a>
+            <a href="../Vista/prestamos.php">Prestamos</a>
+            <a href="../Vista/login.php">Salir</a>
+        </nav>
+        <div>
+            <a href="../Vista/nuevoPresta.php">Insertar Prestamos</a>
+            <a href="../Vista/buscPresta.php">Buscar Prestamos</a>
+        </div>
+    </header>
 
 
 
