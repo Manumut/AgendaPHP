5a3c4bf3e04516e0cce90bbe31634bc2547cb6b4
unknown <manuel.presentaciongr@gmail.com>
2025-02-11 12:43:07 +0100

    cambios en clase amigos

--- Modelo/class.juegos.php
+++ Modelo/class.juegos.php
@@ -1,14 +1,24 @@
 <?php
     require_once("../Modelo/class.db.php");
     class Juego{
-        public $conn;
+        public $con;
+        public $id_jue;
+        public $dueño;
+        public $nombre;
+        public $plataforma;
+        public $lanzamiento;
         public function __construct(){
-            $this->conn=new db();
+            $this->con=new db();
+            $this->id_jue;
+            $this->dueño;
+            $this->nombre;
+            $this->plataforma;
+            $this->lanzamiento;   
         }
         //Obtener los juegos del usuario
         public function obtenerJuegos(int $id_usuario){
             $sentencia ="SELECT id_juego, titulo, plataforma, anio_lanzamiento, foto FROM juegos WHERE id_usuario= ?;";
-            $consulta=$this->conn->__get("conn")->prepare($sentencia);
+            $consulta=$this->con->__get("con")->prepare($sentencia);
             $consulta->bind_param("i",$id_usuario);
             $consulta->bind_result($id_juego, $titulo, $plataforma, $anio_lanzamiento, $foto);
             $juegos = array();
@@ -22,14 +32,14 @@
         //Insertar un nuevo juego para el usuario
         public function insertar($id_usuario, $titulo, $plataforma, $anio_lanzamiento, $foto){
             $sentencia = "INSERT INTO juegos (id_usuario, titulo, plataforma, anio_lanzamiento, foto) VALUES (?, ?, ?, ?,?)";
-            $consulta = $this->conn->__get("conn")->prepare($sentencia);
+            $consulta = $this->con->__get("con")->prepare($sentencia);
             $consulta->bind_param("issss", $id_usuario, $titulo, $plataforma, $anio_lanzamiento, $foto);
             return $consulta->execute();
         }
         //Obtener el juego en especifico
         public function obtenerPorId(int $id_juego) {
             $sentencia = "SELECT titulo, plataforma, anio_lanzamiento, foto FROM juegos WHERE id_juego = ?";
-            $consulta = $this->conn->__get("conn")->prepare($sentencia);
+            $consulta = $this->con->__get("con")->prepare($sentencia);
             $consulta->bind_param("i", $id_juego);
             $consulta->execute();
             $resultado = $consulta->get_result();
@@ -40,7 +50,7 @@
         //Modificar el juego seleccionado
         public function actualizar($id_juego, $titulo, $plataforma, $anio_lanzamiento, $foto) {
             $sentencia = "UPDATE juegos SET titulo = ?, plataforma = ?, anio_lanzamiento = ?, foto = ? WHERE id_juego = ?";
-            $consulta = $this->conn->__get("conn")->prepare($sentencia);
+            $consulta = $this->con->__get("con")->prepare($sentencia);
             $consulta->bind_param("ssssi", $titulo, $plataforma, $anio_lanzamiento, $foto, $id_juego);
             $resultado = $consulta->execute();
             $consulta->close();
@@ -59,7 +69,7 @@
             $sentencia = "SELECT id_juego, titulo, plataforma, anio_lanzamiento, foto 
                     FROM juegos 
                     WHERE id_usuario = ? AND (titulo LIKE ? OR plataforma LIKE ?)";
-            $consulta = $this->conn->__get("conn")->prepare($sentencia);
+            $consulta = $this->con->__get("con")->prepare($sentencia);
             $likeBusqueda = "%" . $busqueda . "%";
             $consulta->bind_param("iss", $id_usuario, $likeBusqueda, $likeBusqueda);
             $consulta->execute();
